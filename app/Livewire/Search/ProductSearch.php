64d88c7b4057a6a31f4bc854860a63e844f58fd9
Jude mpoyo <mpoyojude0@gmail.com>
2025-03-25 14:28:46 +0200

    implement product search functionality; add live search feature with results display

--- app/Livewire/Search/ProductSearch.php
+++ app/Livewire/Search/ProductSearch.php
@@ -3,9 +3,26 @@
 namespace App\Livewire\Search;
 
 use Livewire\Component;
+use App\Models\Product;
 
 class ProductSearch extends Component
 {
+    public $query = '';
+    public $products = [];
+
+    public function updatedQuery()
+    {
+        $this->search();
+    }
+
+    public function search()
+    {
+        $this->products = Product::where('name', 'like', '%'.$this->query.'%')
+            ->orWhere('description', 'like', '%'.$this->query.'%')
+            ->take(10)
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.search.product-search');
